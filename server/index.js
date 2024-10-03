e5fef77eb3368d37cb693540f82047b33a8c8b5d
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-10-03 11:47:19 +0430

    MERN Stack Full-Stack CRUD App

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import Dotenv from "dotenv"
+
+const app = express();
+app.use(bodyParser.json());
+Dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+        .connect(MONGOURL)
+        .then(()=>{
+            console.log("DB Connected Successfully.")
+            app.listen(PORT,()=>{
+                console.log('Server is Running on port:${PORT} ');
+            });
+        })
+        .catch((error)=>console.log(error));
