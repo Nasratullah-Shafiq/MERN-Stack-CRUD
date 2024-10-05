2c54515389c2cad5f8753febd28193a671b5cb1e
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-10-05 10:37:36 +0430

    New files added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -27,3 +27,31 @@ export const create = async(req, res)=>{
         res.status(500).json({errorMessage:error.message});
     }
  }
+ export const getUserById = async(req, res)=>{
+    try{
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if(!userExist){
+            return res.status(404).json({ message: "User Data not found." });
+        }
+        res.status(200).json(userExist);
+    }catch(error){
+        res.status(500).json({ errorMessage: error.message });
+    }
+ };
+
+ export const update = async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if(!userExist){
+            return res.status(404).json({ message: "User not Found: "});
+        }
+        await User.findByIdAndUpdate(id, req.body, {
+            new:true
+        })
+        res.status(200).json(updateData);
+    }catch(error){
+        res.status(500).json({ errorMessage: error.message });
+    }
+ }
